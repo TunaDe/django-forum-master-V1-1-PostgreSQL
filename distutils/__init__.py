6a46484ca4441b81167913561a4914e5ff971740
TunaDe <99804097+TunaDe@users.noreply.github.com>
2026-01-12 17:56:24 +0300

    Delete distutils/__init__.py

--- distutils/__init__.py
+++
@@ -1,3 +0,0 @@
-# Compatibility shim for Python 3.12+ where distutils is removed.
-# Only exposes what Django 3.1 imports: distutils.version.LooseVersion
-from .version import LooseVersion  # noqa: F401
