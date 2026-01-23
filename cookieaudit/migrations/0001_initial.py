7fdee9fdd31f8e0c060b65d9d931d5e2caba5b55
TunaDe <99804097+TunaDe@users.noreply.github.com>
2026-01-23 13:52:30 +0300

    Refactor migration file for CookieEvent model

--- cookieaudit/migrations/0001_initial.py
+++ cookieaudit/migrations/0001_initial.py
@@ -1,16 +1,10 @@
-# Generated for Django 3.1.x
 from django.db import migrations, models
 import django.db.models.deletion
 from django.conf import settings
 
 class Migration(migrations.Migration):
-
     initial = True
-
-    dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-    ]
-
+    dependencies = [migrations.swappable_dependency(settings.AUTH_USER_MODEL)]
     operations = [
         migrations.CreateModel(
             name='CookieEvent',
@@ -22,13 +16,11 @@ class Migration(migrations.Migration):
                 ('session_key', models.CharField(blank=True, max_length=40)),
                 ('client_ip', models.GenericIPAddressField(blank=True, null=True)),
                 ('user_agent', models.TextField(blank=True)),
-                ('cookie_name', models.CharField(db_index=True, max_length=128)),
+                ('cookie_name', models.CharField(max_length=128, db_index=True)),
                 ('cookie_value_preview', models.CharField(blank=True, max_length=128)),
                 ('value_sha256', models.CharField(blank=True, max_length=64)),
                 ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
             ],
-            options={
-                'ordering': ['-created_at'],
-            },
+            options={'ordering': ['-created_at']},
         ),
     ]
